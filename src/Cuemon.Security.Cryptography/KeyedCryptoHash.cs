d54e9994002c40af424f137856204adf95a3cf1b
Michael Mortensen <michael@geekle.io>
2025-11-26 19:31:49 +0100

    :bug: add null-check validation to computehash input

--- src/Cuemon.Security.Cryptography/KeyedCryptoHash.cs
+++ src/Cuemon.Security.Cryptography/KeyedCryptoHash.cs
@@ -28,6 +28,7 @@ namespace Cuemon.Security.Cryptography
         /// <returns>A <see cref="HashResult" /> containing the computed hash code of the specified <paramref name="input" />.</returns>
         public override HashResult ComputeHash(byte[] input)
         {
+            Validator.ThrowIfNull(input);
             using (var h = Initializer())
             {
                 return new HashResult(h.ComputeHash(input));

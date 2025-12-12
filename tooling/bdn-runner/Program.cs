6f2ca4debb99918b1313fa1d2729332d556269f7
Michael Mortensen <michael@geekle.io>
2025-12-12 02:22:34 +0100

    :package: add bdn-runner tool utilizing benchmarkdotnet

---
+++ tooling/bdn-runner/Program.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using Codebelt.Extensions.BenchmarkDotNet;
+using Codebelt.Extensions.BenchmarkDotNet.Console;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkProgram.Run(args, o =>
+        {
+            o.AllowDebugBuild = BenchmarkProgram.IsDebugBuild;
+            o.ConfigureBenchmarkDotNet(c =>
+            {
+                var slimJob = BenchmarkWorkspaceOptions.Slim;
+                return c.AddJob(slimJob.WithRuntime(CoreRuntime.Core90))
+                    .AddJob(slimJob.WithRuntime(CoreRuntime.Core10_0));
+            });
+        });
+    }
+}

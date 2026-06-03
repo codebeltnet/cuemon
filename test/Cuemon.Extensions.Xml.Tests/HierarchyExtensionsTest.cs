ae8aa880afceabb0f6046f56712e6e83ae7b6965
gimlichael <michael@geekle.io>
2026-06-03 21:37:15 +0200

    🐛 add missing xml namespace import in hierarchy extensions test

--- test/Cuemon.Extensions.Xml.Tests/HierarchyExtensionsTest.cs
+++ test/Cuemon.Extensions.Xml.Tests/HierarchyExtensionsTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml;
 using System.Xml.Serialization;
 using Codebelt.Extensions.Xunit;
 using Cuemon.Extensions.Runtime.Serialization;
